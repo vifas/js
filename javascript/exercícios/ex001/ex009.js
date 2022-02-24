e1ae93d4cd8aab3bbd28c58062d430a1922b1abd
vifas <victorfaustino12@gmail.com>
2022-02-24 14:25:56 -0300

    javascript

---
+++ javascript/exercícios/ex001/ex009.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log (`A velocidade do seu carro é de ${vel}km/h por hora`)
+if(vel>60) {
+    console.log (`você ultrapassou a velocidade permitida multado`)
+}
+console.log (`dirija sempre com cinto de segurança`)
