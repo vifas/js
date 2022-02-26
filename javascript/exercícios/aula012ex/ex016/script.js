01efa6e84d0de20dffad9e02137025d0ecec47b4
vifas <victorfaustino12@gmail.com>
2022-02-26 18:11:15 -0300

    js

--- javascript/exercícios/aula012ex/ex016/script.js
+++ javascript/exercícios/aula012ex/ex016/script.js
@@ -4,15 +4,29 @@ function contar() {
     let pas = document.getElementById(`txtp`)
     let res = document.getElementById(`res`)
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        window.alert(`[ERRO] Faltam dados!`)
+        res.innerHTML = `Impossivel contar!`
+        //window.alert(`[ERRO] Faltam dados!`)
+        
     } else {
-       res.innerHTML = `contando `
+       res.innerHTML = `Contando: `
        let i = Number(ini.value)
        let f = Number(fim.value)
        let p = Number(pas.value)
-
-       for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F449}`
+        if (p <= 0) {
+            window.alert(`Passo invalido Considerando PASSO 1`)
+            p = 1
         }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`    
+            } 
+      
+        } else {
+                for(let c =i; c>=f; c -= p) {
+                    res.innerHTML += ` ${c} \u{1F449}`   
+                }
+            }
+            res.innerHTML += ` \u{1F3C1}`
     }
 }
