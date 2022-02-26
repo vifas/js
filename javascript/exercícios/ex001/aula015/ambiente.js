4f9457cf95d263ff5bac326379732002ede7d2b4
vifas <victorfaustino12@gmail.com>
2022-02-25 21:18:39 -0300

    js

--- javascript/exercícios/ex001/aula015/ambiente.js
+++ javascript/exercícios/ex001/aula015/ambiente.js
@@ -6,3 +6,10 @@ num.sort()
 console.log(`Nosso vetor é o ${num}`)
 console.log(`Nosso vetor tem ${num.length} elementos`)
 console.log(`O primeiro vetor é ${num[0]}`)
+
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado`)
+} else {
+console.log(`O valor 6 está na posição ${pos}`)
+}
--- javascript/exercícios/ex001/aula015/vetornatela.js
+++ javascript/exercícios/ex001/aula015/vetornatela.js
@@ -1,5 +1,12 @@
 let valores = [0,3,4,5,8]
+console.log(valores)
 
-for(let pos=0;pos<num.length;pos++) {
-    console.log(pos)
+/*
+for(let pos=0; pos<valores.length;pos++) {
+    console.log(`A posição ${pos} têm o valor ${valores[pos]}`)
+}
+*/
+
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
