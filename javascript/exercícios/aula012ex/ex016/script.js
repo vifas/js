5ebb316ab98f07d34f87f4dd244f043ee78f0a2f
vifas <victorfaustino12@gmail.com>
2022-02-26 17:41:10 -0300

    js

---
+++ javascript/exercícios/aula012ex/ex016/script.js
@@ -0,0 +1,18 @@
+function contar() {
+    let ini = document.getElementById(`txti`)
+    let fim = document.getElementById(`txtf`)
+    let pas = document.getElementById(`txtp`)
+    let res = document.getElementById(`res`)
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        window.alert(`[ERRO] Faltam dados!`)
+    } else {
+       res.innerHTML = `contando`
+       let i = Number(ini.value)
+       let f = Number(fim.value)
+       let p = Number(pas.value)
+
+       for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
+        }
+    }
+}
