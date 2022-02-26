9b9ff4dac6eda8afd3fdc17b9e9b8f66ff6b6b78
vifas <victorfaustino12@gmail.com>
2022-02-25 21:04:46 -0300

    js

---
+++ javascript/exercícios/ex001/aula015/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,6,7]
+num[3] = 6
+num.push(9)
+num.sort()
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`Nosso vetor tem ${num.length} elementos`)
+console.log(`O primeiro vetor é ${num[0]}`)
---
+++ javascript/exercícios/ex001/aula015/vetornatela.js
@@ -0,0 +1,5 @@
+let valores = [0,3,4,5,8]
+
+for(let pos=0;pos<num.length;pos++) {
+    console.log(pos)
+}
